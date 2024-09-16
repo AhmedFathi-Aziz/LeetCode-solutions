87dc9ddf272c65257963996b17a93bfc792d3c31
Ahmad Fathi <ahmed.fathi.aziz@gmail.com>
2024-09-16 03:03:53 +0300

    diameter of binary tree

---
+++ Problems/Diameter of Binary Tree/main.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int diameter = 0;
+    int depth(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+        int left = depth(root->left);
+        int right = depth(root->right);
+
+        diameter = max(diameter, left + right);
+        return max(left, right) + 1;
+    }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        depth(root);
+        return diameter;
+    }
+};
+
+/*
+    return the maximum depth of the subtree rooted at the current node
+    for each node
+        compute the sum of depths of its left and right subtrees
+        maximize the answer
+*/
