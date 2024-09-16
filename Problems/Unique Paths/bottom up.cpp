9200bf07b1990b527aba4612c0dc69d0c024e2ae
Ahmad Fathi <ahmed.fathi.aziz@gmail.com>
2024-09-16 23:57:33 +0300

    refactor the bottom-up solution

--- Problems/Unique Paths/bottom up.cpp
+++ Problems/Unique Paths/bottom up.cpp
@@ -2,16 +2,23 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         int dp[m][n];
-        for (int i = 0; i < m; i++)
-            dp[i][0] = 1;
-        for (int i = 0; i < n; i++)
-            dp[0][i] = 1;
-
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++)
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+	    dp[0][0] = 1;
+	
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 and j == 0)
+                    continue;
+	        	int value = 0;
+		        if (i)
+                    value += dp[i - 1][j];
+                if (j)
+                    value += dp[i][j - 1];
+                dp[i][j] = value;
+	    	
+	        }
         }
         return dp[m - 1][n - 1];
     }
 };
 // bottom-up approach
+
