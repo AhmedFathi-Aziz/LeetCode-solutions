38dd4cd1a85b01a5c77d232270e34c17e665c303
Ahmad Fathi <ahmed.fathi.aziz@gmail.com>
2024-10-22 22:48:38 +0300

    refactor

--- Problems/String to Integer (atoi)/main.cpp
+++ Problems/String to Integer (atoi)/main.cpp
@@ -1,39 +1,26 @@
 class Solution {
 public:
-
     int myAtoi(string s) {
         int i = 0;
-        int len = s.size();
-        for (; i < len; i++) {
-            if (s[i] == ' ')
-                continue;
-            else
-                break;
-        }
+        for (; s[i] == ' ';)
+            ++i;
         int sign = 1;
         if (s[i] == '+')
             ++i;
         else if (s[i] == '-')
-            ++i, sign = -1;
-        
-        for (; i < len; i++) {
-            if (s[i] == '0')
-                continue;
-            else
-                break;
-        }
-        long long out = 0;
-        while (i < len) {
-            if (!isdigit(s[i]))
-                break;
-            int number = s[i] - '0';
-            out = out * 10 + number;
-            if (sign * out > INT_MAX)
-                return INT_MAX;
-            if (sign * out < INT_MIN)
-                return INT_MIN;
+            sign *= -1,
             ++i;
+        long long answer = 0;
+        while (isdigit(s[i])) {
+            answer = answer * 10 + s[i] - '0';
+            ++i;
+
+            if (sign * answer > INT_MAX) 
+                return INT_MAX;
+            if (sign * answer < INT_MIN)
+                return INT_MIN;
         }
-        return sign * out;
+        answer *= sign;
+        return answer;
     }
 };
